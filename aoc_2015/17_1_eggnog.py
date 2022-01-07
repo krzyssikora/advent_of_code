ec967420113ecddf162c80e3f1981d1698a2c9ef
Krzysztof Sikora <krzyssikora@gmail.com>
2022-01-07 11:23:47 +0100

    2015 day 17 solutions added

---
+++ aoc_2015/17_1_eggnog.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def get_capacities(my_file):
+    return sorted([int(line.strip()) for line in open(my_file).readlines()], reverse=True)
+
+
+def how_many_combinations(capacities, amount):
+    many = 0
+    for combination in range(1, 2 ** len(capacities)):
+        tmp_list = list()
+        for i in range(len(capacities)):
+            if 2 ** i & combination:
+                tmp_list.append(capacities[i])
+        if sum(tmp_list) == amount:
+            many += 1
+    return many
+
+
+def main(my_file):
+    capacities = get_capacities(my_file)
+    print(how_many_combinations(capacities, 150))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "17_input.txt"
+    main(filename)
